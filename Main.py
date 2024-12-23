b98feee326348a408c75a2470dfc2daa097020ba
Raul <twayanaraul@gmail.com>
2024-12-23 20:40:08 +0545

    Changed topplayer to true

--- Main.py
+++ Main.py
@@ -1,11 +1,4 @@
 import Game
-import matplotlib.pyplot as plt
-import numpy as np
 
-MainGame = Game.Game(top_player=False)
+MainGame = Game.Game(top_player=True)
 MainGame.mainLoop()
-xaxis = np.array([2, 8])
-# Y axis parameter:
-yaxis = np.array([4, 9])
-plt.plot(xaxis, yaxis)
-plt.show()
